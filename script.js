073bf8df9078766ea5422f627c0a21d7334eb6b4
ruanhackk <ruan.hack@escola.pr.gov.br>
2025-09-11 11:03:00 -0300

    okl

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const reiniciarBtn = document.getElementById('bth-reiniciar')
+const avanca = document.querySelectorAll('.btn-proximo');
+const reiniciarBtn = document.getElementById('btn-reiniciar')
 
+avanca.forEach(button=>{
+button.addEventListener('click',function( ){
+const atual= document. querySelector('.ativo');
+const proximoPasso = 'passo-'+this.getAttribute('data-proximo');
 
-avanca.forEach(button =>{
-button.addEventListener('click', function() {
-const atual= document.querySelector('.ativo');
-const proximoPasso= 'passo-'+this.getAttribute('data-proximo');
-
-
-atual.classList.remover('ativo');
-const proximoElemento= documnto.getElementByid(proximopasso)
+atual.classList.remove('ativo');
+const proximoElemento= document.getElementById(proximoPasso);
 
 if (proximoElemento){
-    proximoElemento.classList.add('ativo')
-}else}
- consol error(`Elemento com ID "${proximoPasso}"não encontrado.`)
+proximoElemento.classList.add('ativo');
+} else {
+console.error(`Elemento com ID "${proximoPasso}"não encontrado.`)
+}
 
- });
+
 });
+});
+
+//reniciar o jogo ao clicar no botão de renicio
+if (reiniciarBtn) {
+      reinicioBtn.addEventListener('click',()=>{
+      atual.classList.remover('ativo');
+      documento.getElementById('passo-0').classList.add('ativo')
+
+      });
+
+
+
+}
